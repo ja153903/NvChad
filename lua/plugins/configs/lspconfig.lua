7ad25e38e2c661dac68944092e51ef3876d37a39
ja153903 <jaimeabbariao@gmail.com>
2022-06-22 23:50:11 -0400

    fix: formatting

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -12,8 +12,8 @@ local utils = require "core.utils"
 require "ui.lsp"
 
 M.on_attach = function(client, bufnr)
-   client.resolved_capabilities.document_formatting = false
-   client.resolved_capabilities.document_range_formatting = false
+   client.resolved_capabilities.document_formatting = true
+   client.resolved_capabilities.document_range_formatting = true
 
    local lsp_mappings = utils.load_config().mappings.lspconfig
    utils.load_mappings({ lsp_mappings }, { buffer = bufnr })
